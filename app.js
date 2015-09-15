e72d4d8b15697a13d2def3a95e1fa06e7a63e697
Pat <patricia.ann2412@gmail.com>
2015-09-15 00:19:06 -0400

    fixed spacing and indention - clean up. getOneAlbum function and express call updated.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//reqire underscore lib
+//require underscore lib
 var _ = require("underscore");
 
 
@@ -7,9 +7,12 @@ var express = require('express')
 var app = express()
 
 
-// // app.get('/', function (req, res) {
-// //   res.send('Hello seewa World! cccccc')
-// // })
+/*
+app.get('/', function (req, res) {
+  res.send('Hello seewa World! dasfadafa')
+})
+*/
+
 
 
 var server = app.listen(3000, function () {
@@ -18,7 +21,7 @@ var server = app.listen(3000, function () {
   var port = server.address().port
 
   console.log('Example app listening at http://%s:%s', host, port)
-
+   
 })
 
 
@@ -28,54 +31,78 @@ var server = app.listen(3000, function () {
 var MongoClient = require('mongodb').MongoClient,
 	db = MongoClient.db,
 	ObjectID = require('mongodb').ObjectID;
-
-
 	
 	
-// Connection URL & DB
+
+
+
+// Connection for Mongodb: URL & DB
 var url = 'mongodb://localhost:27017/seewa';
+
 
 // Use connect method to connect to the Server 
 MongoClient.connect(url, function(err, db) {
 	if(!err){
 		console.log("Connected correctly to server");
-		}else{
-			console.log("Not connected correctly to server");
-		}
-  
+	}
+	else{
+		console.log("Not connected correctly to server");
+	}
+
+
 	
 	
 	
 	
+	
+var galleries = db.collection( 'galleries' );
+
 //Express
 //Express 
 
 
-//Get list of albums w/cover photo url for home page
-app.get('/', function( req, res ){
-	 getAlbums( function( galleryCallback ){
-	res.send( JSON.stringify( galleryCallback, '\t' ) );	
+// Get list of albums w/cover photo url for home page
+ app.get('/:allAlbums', function( req, res ){
+ 	 getAlbums( function( galleryCallback ){
+ 	res.send( JSON.stringify( galleryCallback, '/t' ) );	
 	 });
-} )	
+ } )
+	
+	
+ 
+ 
+ 
+	
+// TODO How to test express with end points that are not '/'
+// //Get one album by ID
+//  555566334e9e595e1408b4ba Mt Washington album ID for reference
+// TODO STATUS: express results-nul. Function works.
+
+   app.get('/allAlbums/:album_ID', function( req, res ){
+	  var albumID = req.params.album_ID;
+	
+	   getOneAlbum(  albumID, function( albumResults ){
+			res.send( JSON.stringify( albumResults, '/t' ) );
+		} );
+	});	
+
+
+	
+	
+ 
+ 
+	
+	// Albums section
+	// Albums section
+
+	//TODO ****REMINDER: When getting a url that nees to carry params with it use /:paramGoesHere Exam- localhost:3000/albums/:#goesHere 
+	//TODO **** Add consolde.log labels for debuging to know where error is located
+	
+	
+	
 	
 
-
-
-
-
-
-
-
-// Albums section
-// Albums section
-
-var galleries = db.collection( 'galleries' );	
-
-
-
-
-
-//Get a list of all albums with cover photo
+	//Get a list of all albums with cover photo
 	function getAlbums( galleryCallback ) {
 		galleries.find( { }, { name: 1, coverPhoto: 1 } ).toArray( function( err, galleryResults ) {
 			
@@ -85,9 +112,9 @@ var galleries = db.collection( 'galleries' );
 				}
 				
 			console.log( galleryResults );
-			galleryCallback( galleryResults )
-				
-			}else{
+			galleryCallback( galleryResults )	
+			}
+			else{
 				console.log( 'Get albums error.' );
 				return galleryCallback( false );
 			}	
@@ -101,27 +128,28 @@ var galleries = db.collection( 'galleries' );
 	
 	 
 	 
-	//Function: Get one selected album by ID
-	function getOneAlbum ( albumId ){
-		
-			galleries.findOne( { '_id': albumID }, function( err, album)  {
+	
+	
+	
+
+
+	function getOneAlbum ( albumID, albumCallback ){
 				
-				if( !err ){
-					console.log( album );
-					return album;
-				}else{
-					//console.log( 'Error getting selected album' );
-					return false;
-				}
+		galleries.findOne( { '_id': new ObjectID( albumID ) }, function( err, albumResults)  {
+			
+			if( !err ){
+				console.log( 'getOneAlbum function', albumResults );
+				albumCallback( albumResults );
+			}
+			
+			else{
+				console.log( 'Error getting selected album' );
+				return albumCallback( false );
+			}
 		});
-		
 	};
-	
-	//getOneAlbum( albumID );
 
 	
-	
-	//getOneAlbum( albumID );
 
 		
 	
@@ -129,25 +157,28 @@ var galleries = db.collection( 'galleries' );
 	
 	
 	
-	//Add a new album to collection w/empty photo array
+	
 	//Function: Add a new album to collection w/empty photo array
 	var newAlbum = 'No PhotoID Test';
-	var c_photo = './images/one_JPG';
+
 	
 	function addAlbum ( name ){
 		
 		galleries.insert( 
-			{ name: newAlbum, coverPhoto: c_photo, photos:  [   ]  },	 { w: 1 }, function( err, result ) {
-					if( !err ){
-						console.log( "addition successful." + result[0]._id );
-					}else{
-						console.log( newAlbum +' has not been created.' );
-					}
+			{ name: newAlbum, coverPhoto: "", photos:  [   ]  },	 
+			{ w: 1 }, 
+			function( err, result ) {
+				if( !err ){
+					console.log( "addition successful." + result[0]._id );
+				}
+				else{
+					console.log( newAlbum +' has not been created.' );
+				}
 			}
 		);
 	}
 
-	//addAlbum( newAlbum );
+//addAlbum( newAlbum );
 
 
 	
@@ -155,34 +186,36 @@ var galleries = db.collection( 'galleries' );
 	
 	
 	
-	//Remove an album by ID
-	var removeAlbum = new ObjectID( '55089cc4acf8da4287858b12' );
+	//Function: Remove an album by ID
+	var removeAlbum = new ObjectID( '557af832643caba210bbe086' );
 
 		function deleteAlbum ( removeAlbum ){
 
-			galleries.remove( {'_id': removeAlbum },  {w:1 },  function(err, result) {
-				if( !err ){
-					console.log('Album removed' + result);	
-				}else{
-					console.log( 'Error removing album.' );
+			galleries.remove( {'_id': removeAlbum },  
+				{w:1 },  
+				function(err, result) {
+					if( !err ){
+						console.log('Album removed' + result);	
+					}
+					else{
+						console.log( 'Error removing album.' );
+					}
 				}
-			});	
-	}	
+			);	
+		}	
 
-	deleteAlbum( removeAlbum );
+// 	deleteAlbum( removeAlbum );
+	
 	
 
-
-
+	
 	
 	
 // Photos Section	
 // Photos Section
-
-
-
 	
-	//TODO Set Cover photo - needs js
+	
+	// 	//TODO Set Cover photo - needs js
 	var a_id = new ObjectID( '55556823e70002f114c76a45' );
 	var coverPhoto =   './images/two_JPG';
 	
@@ -190,16 +223,17 @@ var galleries = db.collection( 'galleries' );
 	 { '_id': a_id }, 
 	 { $set: 
 		 { coverPhoto: coverPhoto } },
-		 { w:1 }, function( err, result ){	
-		console.log('Cover photo set ' + result );
-	} );
+		 { w:1 }, 
+		 function( err, result ){	
+			console.log('Cover photo set ' + result );
+		} 
+	);
 	
 	
 	 
 	 
 	
-
-	// Remove a  photo
+	// Function: Remove a  photo
 	var a_ID = new ObjectID('557afdbadf4d7bd4113a37d7'); 
 	var photoToRemove = './images/two_JPG'
 	
@@ -209,49 +243,54 @@ var galleries = db.collection( 'galleries' );
 			
 			{ _id: a_ID },
 			{ $pull: { photos: { photoURL: photoToRemove } } },
-				{ w: 1 },  function( err, result ){
-			if( !err ){
-				console.log( 'photo removed' + result );
-			}else{
-				console.log( "Error, photo was not removed." );
-			}
+			{ w: 1 },  	
+			function( err, result ){
+				if( !err ){
+					console.log( 'photo removed' + result );
+				}
+				else{
+					console.log( "Error, photo was not removed." );
+				}
 		} );
 	}
+
 	
-	//removePhoto( a_ID, photoToRemove );
+//  	removePhoto( a_ID, photoToRemove );
 	
 	
 	
 		
-		
 	
-	//Rename album by album id
+	
+	//Function: Rename album by album id
 	var rename_Album = new ObjectID('55556823e70002f114c76a45');
 	var newName = 'New new name';
 	
 	function renameAlbum( albumID, newName ){	
 	
 		galleries.update( 
-			{ _id: rename_Album },  { $set: { 'name': newName } },   function(err, result){
+			{ _id: rename_Album },  { $set: { 'name': newName } },   
+			function(err, result){
 				if( !err ){
-				console.log( 'Album successfully renamed. ' + result );
-				}else{ 
+					console.log( 'Album successfully renamed. ' + result );
+				}
+				else{ 
 					console.log( 'Error renaming album, Please try again later.' );
 				}
 			}	
 		)	
 	}
 
-	renameAlbum( rename_Album, newName );	
+// 	renameAlbum( rename_Album, newName );	
 
 									
 
 	
 	
 	
-	
-	//Update, add new photo
-		var  myAlbumID= new ObjectID( '557afdbadf4d7bd4113a37d7' );
+	 
+	//Function: Update, add new photo
+	var  myAlbumID= new ObjectID( '557afdbadf4d7bd4113a37d7' );
 	var url =  './images/two_JPG';
 	
 	function addPhoto( albumID, photoURL ){
@@ -268,10 +307,12 @@ var galleries = db.collection( 'galleries' );
 					}	
 				}	
 			},
-			{ w: 1 }, function( err, result ){
+			{ w: 1 },
+			function( err, result ){
 				if( !err ){
 					console.log( 'Photo successfully added. ' + result );
-				}else{
+				}
+				else{
 					console.log( 'Photo addition failed.' );
 				}
 			}
@@ -280,12 +321,11 @@ var galleries = db.collection( 'galleries' );
 
 // 	addPhoto( myAlbumID, url );
 	
-	
-
 
 
 	
-//WORKS... Edit photo: description
+	
+//Function: Edit photo: description
 var  myAlbumID = new ObjectID( '55556823e70002f114c76a45' );
 var myURL =  './testFive url.';	
 var newDescription = ' dfsaf Added / edited comment.';
@@ -295,23 +335,30 @@ function editDescript( albumID, url, text ){
 	galleries.update(  
 		{ _id:		
 	 myAlbumID,  "photos.url": myURL },
-		{ $set:  { "photos.$.description": newDescription  } },  { w: 1 }, function( err, result ){
+		{ $set:  { "photos.$.description": newDescription  } }, 
+		{ w: 1 },
+		function( err, result ){
 			if( !err ){
 				console.log( 'Photo description has been successfully changed. ' + result );
-			}else{
+			}
+			else{
 				console.log( 'Error changing photo description.' );
 			}
 		}
 	)
 }
-editDescript( myAlbumID, myURL, newDescription );
+// editDescript( myAlbumID, myURL, newDescription );
 	
-	
-	
-	
+
+
+
+
+
 
 /// comment section	
 /// comment section
+
+
 
 
 //db.createCollection( 'comments' );
@@ -329,7 +376,7 @@ function getAllComments(){
 	} );
 }
 
-//getAllComments();
+//getAllComments( );
 
 
 
@@ -342,8 +389,10 @@ var posTime = Date() ;
 var comment = "New new new";
 
 function addComment(photoId, text ){
-	comments.insert( { photoURL: photo_url, posted: posTime,  comment: comment } ,  {  w: 1 }, function( err, result ){
-		console.log( 'Comment successullly added. ' + result  );
+	comments.insert( { photoURL: photo_url, posted: posTime,  comment: comment } , 
+		 {  w: 1 }, 
+		 function( err, result ){
+			console.log( 'Comment successullly added. ' + result  );
 			console.log( "New comment was successfully added."  + result );
 		} 
 	);
@@ -357,39 +406,44 @@ function addComment(photoId, text ){
 	
 	
 	
-	
-
 //Remove a comment
 var commentID = new ObjectID("55614924a7b2a0662173cd82");
 
 function removeComment( commentID ){
 
-	comments.remove( { _id: commentID }, { w: 1 }, function( err, result ){
-		console.log( result );
+	comments.remove( { _id: commentID }, 
+		{ w: 1 }, 
+		function( err, result ){
+			console.log( result );
 		} 
 	);
 }
-removeComment( commentID );
+// removeComment( commentID );
 		
 
 
 
-
-
+		
+		
+		
+		
 
 //Edit comment text
 var com_ID = new ObjectID("5561492f12be9d7321bd654f");
 var newText = "New edited comment";
 
 function editComment( commentID, newText ){
-	comments.update( { "_id": com_ID }, { $set: { "text": newText } }, { w: 1 }, function( err, result ){
-		if( ! err ){
-			console.log( result + "text update successful" );
+	comments.update( { "_id": com_ID }, { $set: { "comment": newText } }, 
+		{ w: 1 },
+		function( err, result ){
+			if( ! err ){
+				console.log( result + "text update successful" );
 			}
 		}
 	);
 }
-editComment( com_ID, newText );
+// editComment( com_ID, newText );
+
 
 
 
@@ -409,30 +463,18 @@ function getComments4one( photoURL ){
 		});	
 	});
 }
-getComments4one( photoURL );
-	
-	
+// getComments4one( photoURL );
 
 
 
 
 
 
-	
 //db.close();
 });
 
 
-
-
-
-
-
-
-
-
-
-
+	
 
 
 
